f84661d4b537d03cc8dbe831b78481e62b5c809e
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-20 23:54:43 +0700

    реализована функция заполнения массива случайными целыми числами

--- Home_work_5.8/Program.cs
+++ Home_work_5.8/Program.cs
@@ -16,6 +16,23 @@ namespace Home_work_5._8
             // 2 3 5 9
             // 2 4 4 8
 
+            int m = 4, n = 4;
+            int[,] array = new int[m, n];
+
+            FillArrayAndOutput(array);
+
+            void FillArrayAndOutput(int[,] ArrayInFunction)
+            {
+                for (int i = 0; i < ArrayInFunction.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ArrayInFunction.GetLength(1); j++)
+                    {
+                        ArrayInFunction[i, j] = new Random().Next(11);
+                        Console.Write(ArrayInFunction[i, j] + "\t");
+                    }
+                    Console.Write("\n");
+                }
+            }
         }
     }
 }

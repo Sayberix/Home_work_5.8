9ef7ef27822713653ad0d522ee6e57fca5ff5280
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-20 23:34:11 +0700

    Начало выполнения дз 5.8

---
+++ Home_work_5.8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Home_work_5._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Начало выполнение дз к семинару по теме "Как не нужно писать код. Часть 2" - 5.8
+        }
+    }
+}

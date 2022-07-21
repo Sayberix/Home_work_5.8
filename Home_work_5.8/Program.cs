15f3c31a5992a2bcfaab4af96d8b1a0fe4620a6e
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-21 23:33:26 +0700

    задача 5.8.3 - выполнена с дополнительной функцией вывода массива - OutputArray

--- Home_work_5.8/Program.cs
+++ Home_work_5.8/Program.cs
@@ -24,7 +24,56 @@ namespace Home_work_5._8
             // 56 8 4 24
             // 10 6 24 49
 
+            int m = 5, n = 4;
+            int[,] firstArray = new int[m, n];
+            int[,] secondArray = new int[m, n];
 
+            FillArray(firstArray);
+            FillArray(secondArray);
+            Console.WriteLine("Исходный первый массив:");
+            OutputArray(firstArray);
+            Console.Write("\n");
+            Console.WriteLine("Исходный второй массив:");
+            OutputArray(secondArray);
+            Console.Write("\n");
+            Console.WriteLine("Массив с результатом умножения двух исходных массивов:");
+            OutputArray(ArrayMultiplication(firstArray, secondArray));
+
+            // Функция заполнения массива
+            void FillArray(int[,] ArrayInFunction)
+            {
+                for (int i = 0; i < ArrayInFunction.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ArrayInFunction.GetLength(1); j++)
+                    {
+                        ArrayInFunction[i, j] = new Random().Next(11);
+                    }
+                }
+            }
+
+            // Функция вывода массива
+            void OutputArray(int[,] ArrayInFunction)
+            {
+                for (int i = 0; i < ArrayInFunction.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ArrayInFunction.GetLength(1); j++)
+                    {
+                        Console.Write(ArrayInFunction[i, j] + "\t");
+                    }
+                    Console.Write("\n");
+                }
+            }
+
+            // Функция произведения элементов двух матриц
+            int [,] ArrayMultiplication(int[,] firstArray, int[,] secondArray)
+            {
+                int lengthRow = firstArray.GetLength(0); int lengthColumn = firstArray.GetLength(1);
+                int[,] resaultMultiplication = new int[lengthRow, lengthColumn];
+                for (int i = 0; i < lengthRow; i++)
+                    for (int j = 0; j < lengthColumn; j++)
+                        resaultMultiplication[i, j] = firstArray[i,j] * secondArray[i,j];
+                return (resaultMultiplication);
+            }
         }
     }
 }
